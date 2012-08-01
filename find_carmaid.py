2244edb2fede947475a3f6f8e13991bea3462929
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2012-08-01 16:10:25 -0500

    Added find_carmaid and find_prop

---
+++ find_carmaid.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+# Display information about a particular CARMA proposal id.
+
+import cpssdb
+import sys
+
+def start(carmaid):
+    Database = cpssdb.cpssdb()
+    
+    cursor = Database.cursor()
+    sql = ("""SELECT * 
+                      FROM (SELECT * FROM `proposals` UNION 
+                            SELECT * FROM `cs_proposals` UNION 
+                            SELECT * FROM `ddt_proposals`) AS t1, `users`
+                      WHERE t1.carmaid='%s' AND
+                            `t1`.`user`=`users`.`email`""" % carmaid)
+
+    cursor.execute(sql)
+    while (1):
+        result = cursor.fetchone()
+        if (result != None):
+            print result
+        else:
+            break
+
+    cursor.close()
+    Database.close()
+
+
+if __name__ == "__main__":
+    start(sys.argv[1])
---
+++ find_prop.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+# Find all proposals by a particular user.
+import cpssdb
+import sys
+
+def start(emails):
+    string = "IN('" + "', '".join(emails) + "')"
+
+    Database = cpssdb.cpssdb()
+    
+    #main
+    cursor = Database.cursor()
+    cursor.execute("""SELECT * 
+                      FROM (SELECT * FROM `proposals` UNION 
+                            SELECT * FROM `cs_proposals` UNION 
+                            SELECT * FROM `ddt_proposals`) AS t1
+                      WHERE `t1`.`user` %s
+                      ORDER BY `t1`.`carmaid`""" % string)
+
+    while (1):
+        result = cursor.fetchone()
+        if (result != None):
+            print result
+        else:
+            break
+
+    cursor.close()
+    Database.close()
+
+if __name__ == "__main__":
+    start(sys.argv[1:])
